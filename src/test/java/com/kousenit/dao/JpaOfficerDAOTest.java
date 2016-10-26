acde7859bda2962842dac7bd74271a4f14b50748
Kenneth Kousen <ken.kousen@kousenit.com>
2016-10-26 12:49:01 -0400

    Update all tests and implement Jpa repo

--- src/test/java/com/kousenit/dao/JpaOfficerDAOTest.java
+++ src/test/java/com/kousenit/dao/JpaOfficerDAOTest.java
@@ -2,17 +2,15 @@ package com.kousenit.dao;
 
 import com.kousenit.entities.Officer;
 import com.kousenit.entities.Rank;
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.annotation.Resource;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -24,12 +22,9 @@ import static org.junit.Assert.*;
 @SpringBootTest
 @RunWith(SpringRunner.class)
 @Transactional
-public class JdbcOfficerDAOTest {
-    @Autowired @Qualifier("jdbcOfficerDAO")
+public class JpaOfficerDAOTest {
+    @Autowired @Qualifier("jpaOfficerDAO")
     private OfficerDAO dao;
-
-    @Autowired
-    private JdbcTemplate jdbcTemplate;
 
     private List<Officer> officers =
             Arrays.asList(new Officer(Rank.CAPTAIN, "James", "Kirk"),
@@ -38,26 +33,8 @@ public class JdbcOfficerDAOTest {
                     new Officer(Rank.CAPTAIN, "Benjamin", "Sisko"),
                     new Officer(Rank.CAPTAIN, "Jonathan", "Archer"));
 
-    @Before
-    public void setUp() {
-        jdbcTemplate.execute("DROP TABLE IF EXISTS officers");
-        jdbcTemplate.execute("CREATE TABLE officers(\n" +
-                "    id INT NOT NULL AUTO_INCREMENT,\n" +
-                "    rank VARCHAR(20) NOT NULL, first_name VARCHAR(50) NOT NULL,\n" +
-                "    last_name VARCHAR(20) NOT NULL,\n" +
-                "    PRIMARY KEY(id)\n" +
-                ")");
-        officers.stream()
-                .forEach(officer -> dao.save(officer));
-    }
-
-//    @After
-//    public void tearDown() {
-//        jdbcTemplate.execute("DROP TABLE IF EXISTS officers");
-//    }
-
     @Test
-    public void save() throws Exception {
+    public void testSave() throws Exception {
         Officer officer = new Officer(Rank.LIEUTENANT, "Nyota", "Uhuru");
         officer = dao.save(officer);
         assertNotNull(officer.getId());
@@ -68,13 +45,21 @@ public class JdbcOfficerDAOTest {
 
     @Test
     public void findOne() throws Exception {
-        Officer officer = dao.findOne(1);
-        assertNotNull(officer);
-        assertEquals(1, officer.getId().intValue());
+        officers.stream()
+                .forEach(officer -> dao.save(officer));
+        officers.stream()
+                .mapToInt(Officer::getId)
+                .forEach(id -> {
+                    Officer officer = dao.findOne(id);
+                    assertNotNull(officer);
+                    assertEquals(id, officer.getId().intValue());
+                });
     }
 
     @Test
     public void findAll() throws Exception {
+        officers.stream()
+                .forEach(officer -> dao.save(officer));
         List<String> dbNames = dao.findAll().stream()
                 .map(Officer::getLast)
                 .collect(Collectors.toList());
@@ -83,20 +68,28 @@ public class JdbcOfficerDAOTest {
 
     @Test
     public void count() throws Exception {
+        officers.stream()
+                .forEach(officer -> dao.save(officer));
         assertEquals(5, dao.count().longValue());
     }
 
     @Test
     public void delete() throws Exception {
-        IntStream.rangeClosed(1, 5)
+        officers.stream()
+                .forEach(officer -> dao.save(officer));
+        officers.stream()
+                .mapToInt(Officer::getId)
                 .forEach(id -> dao.delete(dao.findOne(id)));
         assertEquals(0, dao.count().longValue());
     }
 
     @Test
     public void exists() throws Exception {
-        IntStream.rangeClosed(1, 5)
-                .forEach(id -> assertTrue(String.format("%d should exist", id), dao.exists(id)));
+        officers.stream()
+                .forEach(officer -> dao.save(officer));
+        officers.stream()
+                .mapToInt(Officer::getId)
+                .forEach(id -> assertTrue(String.format("%d should exist", id),
+                        dao.exists(id)));
     }
-
 }
