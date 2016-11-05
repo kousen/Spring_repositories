304603ecff36b6daec20f8cd14c9436dee15ee16
Kenneth Kousen <ken.kousen@kousenit.com>
2016-11-05 11:31:25 -0400

    Use @Param to enable search in rest respos

--- src/main/java/com/kousenit/dao/OfficerRepository.java
+++ src/main/java/com/kousenit/dao/OfficerRepository.java
@@ -3,11 +3,12 @@ package com.kousenit.dao;
 import com.kousenit.entities.Officer;
 import com.kousenit.entities.Rank;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface OfficerRepository extends JpaRepository<Officer, Integer> {
-    List<Officer> findByRank(Rank rank);
+    List<Officer> findByRank(@Param("rank") Rank rank);
 
-    Officer findByLast(String last);
+    Officer findByLast(@Param("last") String last);
 }
