699dfa04eb574b99a1e85e96b32dd2bd932900d1
Kenneth Kousen <ken.kousen@kousenit.com>
2016-11-05 10:22:06 -0400

    Add actuator to see config props. Also change config file to yml

--- src/test/java/com/kousenit/PersistenceApplicationTests.java
+++ src/test/java/com/kousenit/PersistenceApplicationTests.java
@@ -2,15 +2,24 @@ package com.kousenit;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.stream.Stream;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class PersistenceApplicationTests {
+    @Autowired
+    private ApplicationContext ctx;
 
 	@Test
 	public void contextLoads() {
+        Stream.of(ctx.getBeanDefinitionNames())
+                .filter(name -> name.contains("TransactionManager"))
+                .forEach(System.out::println);
 	}
 
 }
