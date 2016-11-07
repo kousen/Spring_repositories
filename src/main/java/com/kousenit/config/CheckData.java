b265d56599e945e8f1bebaeac406dd7ce84ba08a
Kenneth Kousen <ken.kousen@kousenit.com>
2016-11-06 20:20:41 -0500

    Add command line runner demo

--- src/main/java/com/kousenit/config/CheckData.java
+++ src/main/java/com/kousenit/config/CheckData.java
@@ -1,7 +1,22 @@
 package com.kousenit.config;
 
-/**
- * Created by Ken Kousen on 11/5/16.
- */
-public class CheckData {
+import com.kousenit.dao.OfficerDAO;
+import com.kousenit.entities.Officer;
+import com.kousenit.entities.Rank;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CheckData implements CommandLineRunner {
+    @Autowired
+    private OfficerDAO dao;
+
+    @Override
+    public void run(String... args) throws Exception {
+        dao.findAll().forEach(System.out::println);
+        Officer spock = dao.save(new Officer(Rank.COMMANDER, "", "Spock"));
+        System.out.println(spock);
+        dao.delete(spock);
+    }
 }
